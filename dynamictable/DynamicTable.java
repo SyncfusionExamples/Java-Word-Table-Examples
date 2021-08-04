511c95e0e43538cf3fd7464f2800dd3972491cb2
Dharani Thangarasu <dharani.thangarasu@syncfusion.com>
2021-08-04 15:31:42 +0530

    Added comments

--- dynamictable/DynamicTable.java
+++ dynamictable/DynamicTable.java
@@ -3,56 +3,57 @@ import com.syncfusion.docio.*;
 public class DynamicTable {
 
 	public static void main(String[] args) throws Exception {
-		// Creates an instance of WordDocument class
+		// Creates an instance of WordDocument class.
 		WordDocument document = new WordDocument();
+		// Adds a section to the Word document.
 		IWSection section = document.addSection();
+		// Adds new text to the paragraph.
 		section.addParagraph().appendText("Price Details");
 		section.addParagraph();
-		// Adds a new table into Word document
+		// Adds a new table into Word document.
 		IWTable table = section.addTable();
-		// Adds the first row into table
+		// Adds the first row into table.
 		WTableRow row = table.addRow();
-		// Adds the first cell into first row
+		// Adds the first cell into first row .
 		WTableCell cell = row.addCell();
 		cell.addParagraph().appendText("Item");
-		// Adds the second cell into first row
+		// Adds the second cell into first row.
 		cell = row.addCell();
 		cell.addParagraph().appendText("Price($)");
-		// Adds the second row into table
+		// Adds the second row into table.
 		row = table.addRow(true, false);
-		// Adds the first cell into second row
+		// Adds the first cell into second row.
 		cell = row.addCell();
 		cell.addParagraph().appendText("Apple");
-		// Adds the second cell into second row
+		// Adds the second cell into second row.
 		cell = row.addCell();
 		cell.addParagraph().appendText("50");
-		// Adds the third row into table
+		// Adds the third row into table.
 		row = table.addRow(true, false);
-		// Adds the first cell into third row
+		// Adds the first cell into third row.
 		cell = row.addCell();
 		cell.addParagraph().appendText("Orange");
-		// Adds the second cell into third row
+		// Adds the second cell into third row.
 		cell = row.addCell();
 		cell.addParagraph().appendText("30");
-		// Adds the fourth row into table
+		// Adds the fourth row into table.
 		row = table.addRow(true, false);
-		// Adds the first cell into fourth row
+		// Adds the first cell into fourth row.
 		cell = row.addCell();
 		cell.addParagraph().appendText("Banana");
-		// Adds the second cell into fourth row
+		// Adds the second cell into fourth row.
 		cell = row.addCell();
 		cell.addParagraph().appendText("20");
-		// Adds the fifth row to table
+		// Adds the fifth row to table.
 		row = table.addRow(true, false);
-		// Adds the first cell into fifth row
+		// Adds the first cell into fifth row.
 		cell = row.addCell();
 		cell.addParagraph().appendText("Grapes");
-		// Adds the second cell into fifth row
+		// Adds the second cell into fifth row.
 		cell = row.addCell();
 		cell.addParagraph().appendText("70");
-		// Saves and closes the document instance
+		// Saves and closes the document instance.
 		document.save("Result.docx", FormatType.Docx);
 		document.close();
-
 	}
 }
